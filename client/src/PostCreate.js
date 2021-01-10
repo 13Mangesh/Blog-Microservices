583426f68df3ff65456c9db3c1881c3cc971da8e
13Mangesh <13mangeshpuri@gmail.com>
2021-01-10 15:41:12 +0530

    Basic setup is done

---
+++ client/src/PostCreate.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import React, { useState } from 'react';
+
+const PostCreate = () => {
+	const [title, setTitle] = useState('');
+
+	const onSubmit = async (e) => {
+		e.preventDefault();
+
+		await axios.post('http://localhost:4000/posts', {
+			title,
+		});
+
+		setTitle('');
+	};
+
+	return (
+		<div>
+			<form onSubmit={onSubmit}>
+				<div className="form-group my-3">
+					<label>Title</label>
+					<input
+						value={title}
+						onChange={(e) => setTitle(e.target.value)}
+						className="form-control"
+					></input>
+				</div>
+				<button className="btn btn-primary">Submit</button>
+			</form>
+		</div>
+	);
+};
+
+export default PostCreate;
